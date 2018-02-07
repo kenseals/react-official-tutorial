6967bb0d2ea0590ef633d4a7bbfe835538920621
Ken Seals <kenseals@gmail.com>
2018-02-06 22:21:21 -0500

    Display the location for each move in the format (col, row) in the move history list

--- src/index.js
+++ src/index.js
@@ -48,7 +48,8 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        position: 1
       }],
       stepNumber: 0,
       xIsNext: true
@@ -59,6 +60,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length -1];
     const squares = current.squares.slice();
+    const position = calculatePosition(i);
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -66,6 +68,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        position: position
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -86,7 +89,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + " (" + String(step.position) + ")":
         'Go to game start';
       return (
         <li key={move}>
@@ -125,6 +128,31 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculatePosition(square) {
+  const cols = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
+  const rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
+  let x, y;
+  for (let i = 0; i < cols.length && x === undefined; i++) {
+    if (cols[i].indexOf(square) !== -1) {
+      x = i + 1;
+    }
+  }
+  for (let i = 0; i < rows.length && y === undefined; i++) {
+    if (rows[i].indexOf(square) !== -1) {
+      y = i + 1;
+    }
+  }
+  return [x, y];
+}
 
 function calculateWinner(squares) {
   const lines = [
